dfe5647b2dbcd776b3cdf3aa4941097a2d2939b2
bahar <bahar1377.a@gmail.com>
2021-12-02 10:56:43 +0330

    Add type listOfItemsType and droppableItemInfoType

--- types/componentType.ts
+++ types/componentType.ts
@@ -1,8 +1,17 @@
-export type QuestionRowProps={
+export type QuestionRowPropsTypes={
     item:{
         id:string;
         content:string;
     };
     index:number;
     isDragDisabled:boolean;
+}
+export type listOfItemsType = Array<{
+    id: string, content: string, type: string
+}>
+
+export type droppableItemInfoType={
+    index: number,
+    droppableId: string
+
 }
