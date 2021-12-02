7b9eaf86821b167ffa4aca3cba59318d10fb1fee
bahar <bahar1377.a@gmail.com>
2021-12-02 16:30:56 +0330

    Add english and persian translations.

---
+++ translations/english.ts
@@ -0,0 +1,37 @@
+export const translations = {
+    placeHolder:{
+        welcomePage: {
+            title: 'Welcome Page Place holder',
+        },
+        appreciationPage: {
+            titleText: "Appreciation page place holder",
+        },
+        question:{
+            titleText:"Question placeholder",
+        }
+
+    },
+    welcomePage: {
+        title: 'Welcome Page',
+    },
+    appreciationPage: {
+        TitleText: "Appreciation page",
+    },
+    questions: {
+        multipleChoiceQuestion: {
+            title: "Multiple choice question",
+            confirmButtonText: "OK",
+        },
+        textQuestion: {
+            longAnswer: {
+                titleText: "Long answer",
+            },
+            number: {
+                titleText: "Number",
+            },
+            shortAnswer: {
+                titleText: "Short answer",
+            },
+        },
+    }
+}
---
+++ translations/persian.ts
@@ -0,0 +1,37 @@
+export const translations = {
+    placeHolder:{
+        welcomePage: {
+            titleText: "صفحه‌ خوش‌آمد گویی را از ستون سمت راست به اینجا بکشید",
+        },
+        appreciationPage: {
+            titleText: "صفحه‌ تشکر را از ستون سمت راست به اینجا بکشید",
+        },
+        question:{
+            titleText:"نوع سوال مورد نظر خود را از ستون سمت راست به این جا بکشید",
+        }
+    },
+    welcomePage: {
+        titleText: 'صفحه خوش آمد گویی',
+    },
+    appreciationPage: {
+        TitleText: "صفحه تشکر",
+    },
+    questions: {
+        multipleChoiceQuestion: {
+            titleText:"چند گزینه ای"
+        },
+        textQuestion: {
+            longAnswer: {
+                title:"متنی با پاسخ بلند"
+            },
+            shortAnswer: {
+                title:"متنی با پاسخ کوتاه"
+            },
+            number: {
+                title:"عدد"
+            },
+        },
+
+    }
+}
+
