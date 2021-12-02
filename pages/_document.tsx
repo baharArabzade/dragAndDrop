56d123fa8f22998818f44babee972ec3f4c4e410
bahar <bahar1377.a@gmail.com>
2021-12-02 13:56:30 +0330

    create a custom _document file and call resetServerContext() to make react-beautiful-dnd work with next.js.

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document'
+import {resetServerContext} from "react-beautiful-dnd";
+
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const page = await ctx.renderPage();
+        const initialProps = await Document.getInitialProps(ctx);
+        resetServerContext();
+        return {...initialProps, ...page,};
+    }
+
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>dragAndDrop</title>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
