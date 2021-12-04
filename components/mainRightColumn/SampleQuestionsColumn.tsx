be5dda9a5eda5d262d269474d98f43d59ecdc239
bahar <bahar1377.a@gmail.com>
2021-12-04 11:29:15 +0330

    Add translations.

--- components/mainRightColumn/SampleQuestionsColumn.tsx
+++ components/mainRightColumn/SampleQuestionsColumn.tsx
@@ -3,6 +3,9 @@ import {Draggable, Droppable} from "react-beautiful-dnd";
 import uuid from "uuid/v4";
 //style
 import styles from "styles/components/sampleQuestionsColumn.module.scss";
+//translations
+import {translations as persianTranslations} from "translations/persian";
+import {translations as englishTranslations} from "translations/english";
 
 export const sampleQuestions = [
     {
