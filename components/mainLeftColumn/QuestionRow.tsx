eaf03c13eb13988bc2e8fb312f075ebb758b9662
bahar <bahar1377.a@gmail.com>
2021-12-04 11:38:07 +0330

    Import translations in QuestionRow.tsx

--- components/mainLeftColumn/QuestionRow.tsx
+++ components/mainLeftColumn/QuestionRow.tsx
@@ -4,9 +4,13 @@ import {Draggable} from "react-beautiful-dnd";
 import classes from "styles/components/questionRow.module.scss";
 // types
 import {QuestionRowPropsTypes} from "types/componentType";
-
+//translations
+import {translations as persianTranslations} from "translations/persian";
+import {translations as englishTranslations} from "translations/english";
 
 const QuestionRow = ({item, index,isDragDisabled}:QuestionRowPropsTypes):JSX.Element => {
+    const language = 'english'
+    const translations = (language === 'english'?englishTranslations:persianTranslations)
     return (
         <Draggable
             isDragDisabled={isDragDisabled}
