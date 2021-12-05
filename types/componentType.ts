e09cfbbba782dd4b10dccae2f7e1c0912dd5ce75
bahar <bahar1377.a@gmail.com>
2021-12-05 11:52:53 +0330

    componentsTypes

--- types/componentType.ts
+++ types/componentType.ts
@@ -1,17 +1,29 @@
 export type QuestionRowPropsTypes={
-    item:{
+    question:{
         id:string;
-        content:string;
+        questionType:string
     };
     index:number;
-    isDragDisabled:boolean;
 }
-export type listOfItemsType = Array<{
-    id: string, content: string, type: string
+export type welcomePageDetailsType= Array<{
+  id:string,
 }>
-
+export type appreciationPageDetailsType= Array<{
+    id:string,
+}>
+export type listOfItemsType = Array<{
+    id: string, type: string
+    content:string
+}>
+export type questionsDetailsType=Array<{
+    id:string;
+    questionType:string
+}>
 export type droppableItemInfoType={
     index: number,
     droppableId: string
-
+}
+export type RightNavbarProps = {
+    isWelcomeDragDisable: boolean;
+    isAppreciationDragDisable: boolean;
 }
