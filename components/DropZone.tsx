018c7dd9cb1d61936a317dd663c33ed46e2594e7
bahar <bahar1377.a@gmail.com>
2021-12-15 00:21:22 +0330

    handle nested DropZone. add props to handle addQuestion,andSunQuestion,addWelcomePage,addAppreciationPage logic.

--- components/DropZone.tsx
+++ components/DropZone.tsx
@@ -1,38 +1,63 @@
 import {useState} from 'react';
 import {useDrop} from 'react-dnd';
+import uuid from "uuid/v4";
 //style
 import classes from "styles/index.module.scss"
 //types
-import {dragItemType, DropZonePropsTypes} from "../types/componentType";
-import {QUESTION_TYPE} from "@components/constants";
+import {dragItemType, DropZonePropsTypes} from "types/componentType";
 //translations
-import {translations as englishTranslations} from "../translations/english";
-import {translations as persianTranslations} from "../translations/persian";
+import {translations as englishTranslations} from "translations/english";
+import {translations as persianTranslations} from "translations/persian";
 
-
-
-
-const DropZone = ({addQuestionFunction, children,acceptType}:DropZonePropsTypes):JSX.Element => {
+const DropZone = ({
+                      addQuestionFunction,
+                      addAppreciationPage,
+                      addWelcomePage,
+                      addSubQuestionToGroupQuestion,
+                      children,
+                      acceptType,
+                      type,
+                      className,
+                      groupQuestionId,
+                  }: DropZonePropsTypes): JSX.Element => {
 
     const [language, setLanguage] = useState('persian')
     const translations = (language === 'english' ? englishTranslations : persianTranslations)
-    const [{isOver}, drop] = useDrop(() => ({
+    const [{isOverCurrent}, drop] = useDrop(() => ({
         accept: acceptType,
-        drop(item:dragItemType, monitor) {
-            if(acceptType === QUESTION_TYPE) {
-
-                addQuestionFunction(item.question.questionType)
+        drop(item: dragItemType, monitor) {
+            const dropOnChild = monitor.didDrop();
+            if (dropOnChild) {
+                return;
+            }
+            switch (type) {
+                case 'groupQuestion':
+                    if (item.question.questionType != 'group') {
+                        addSubQuestionToGroupQuestion(item.question, groupQuestionId)
+                    }
+                    return;
+                case 'welcomePage':
+                    addWelcomePage(uuid())
+                    return;
+                case 'appreciationPage':
+                    addAppreciationPage(uuid())
+                    return;
+                default:
+                    addQuestionFunction(item.question.questionType)
+                    return;
             }
         },
         collect: (monitor) => ({
-            isOver: monitor.isOver(),
+            isOverCurrent: monitor.isOver({shallow: true}),
         }),
-    }), );
+    }),);
 
-    return (<div ref={drop} className={classes.form_container}>
-        <div>
+    return (<div ref={drop}>
+        <div className={classes[className]}>
             {children}
-            <div className={classes.place_holder}>{translations.placeHolder[acceptType].titleText}</div>
+            <div
+                className={isOverCurrent ? classes.place_holder_isOver : classes.place_holder}>{translations.placeHolder[type].titleText}
+            </div>
         </div>
     </div>);
 };
