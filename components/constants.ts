8be9d495efc1fc6684c6f4294507b4646c152f47
bahar <bahar1377.a@gmail.com>
2021-12-15 00:17:29 +0330

    Change some constant.

--- components/constants.ts
+++ components/constants.ts
@@ -3,36 +3,48 @@ import uuid from "uuid";
 export const QUESTION_TYPE = <string>"question";
 export const APPRECIATION_TYPE = <string>"appreciationPage";
 export const WELCOME_TYPE = <string>"welcomePage";
-
+export const GROUP_QUESTION_TYPE = <string>"groupQuestion"
 export const RIGHT_NAVBAR_ITEMS = [
 
-  {  id: uuid(),
-    type: WELCOME_TYPE},
-  {
-    id: uuid(),
-    type: QUESTION_TYPE,
-    question: {
-      questionType: "number",
-      id:uuid()
-    }
-  },
-  {
-    id: uuid(),
-    type: QUESTION_TYPE,
-    question: {
-      questionType: "longAnswer",
-      id:uuid()
-    }
-  },
-  {
-    id: uuid(),
-    type: QUESTION_TYPE,
-    question: {
-      questionType: "shortAnswer",
-      id:uuid()
-    }
-  },
-  {   id: uuid(),
-    type: APPRECIATION_TYPE},
+    {
+        id: uuid(),
+        type: WELCOME_TYPE
+    },
+    {
+        id: uuid(),
+        type: QUESTION_TYPE,
+        question: {
+            questionType: "number",
+            id: uuid()
+        }
+    },
+    {
+        id: uuid(),
+        type: GROUP_QUESTION_TYPE,
+        question: {
+            questionType: 'group',
+            id: uuid(),
+        }
+    },
+    {
+        id: uuid(),
+        type: QUESTION_TYPE,
+        question: {
+            questionType: "longAnswer",
+            id: uuid()
+        }
+    },
+    {
+        id: uuid(),
+        type: QUESTION_TYPE,
+        question: {
+            questionType: "shortAnswer",
+            id: uuid()
+        }
+    },
+    {
+        id: uuid(),
+        type: APPRECIATION_TYPE
+    },
 
 ];
