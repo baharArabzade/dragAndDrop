7c0e86d761364a4094facaf2d642cb9b14c696b5
bahar <bahar1377.a@gmail.com>
2021-12-15 00:23:58 +0330

    RightNavbarItem.

--- components/RightNavbarItem.tsx
+++ components/RightNavbarItem.tsx
@@ -1,29 +1,27 @@
 import React, {useState} from "react";
-import { useDrag } from "react-dnd";
+import {useDrag} from "react-dnd";
 //style
-import  classes from "styles/components/rightNavbar.module.scss"
+import classes from "styles/components/rightNavbar.module.scss"
 //types
-import {RightNavbarPropsTypes} from "../types/componentType";
+import {RightNavbarPropsTypes} from "types/componentType";
 //translations
-import {translations as englishTranslations} from "../translations/english";
-import {translations as persianTranslations} from "../translations/persian";
+import {translations as englishTranslations} from "translations/english";
+import {translations as persianTranslations} from "translations/persian";
 
-const RightNavbarItem = ({data,type}:RightNavbarPropsTypes):JSX.Element => {
-  const [language, setLanguage] = useState('persian')
-  const translations = (language === 'english' ? englishTranslations : persianTranslations)
-
-  const [{ opacity }, drag] = useDrag({
-    type:type,
-    item: data,
-    collect: monitor => ({
-      opacity: monitor.isDragging() ? 0.6 : 1
-    })
-  });
-  return (
-    <div className={classes.default_question} ref={drag} style={{ opacity }} key={data.id}>
-
-      {data.question?.questionType?translations.questions[data.question.questionType].titleText:translations[data.type].titleText}
-    </div>
-  );
+const RightNavbarItem = ({data, type}: RightNavbarPropsTypes): JSX.Element => {
+    const [language, setLanguage] = useState('persian')
+    const translations = (language === 'english' ? englishTranslations : persianTranslations)
+    const [{opacity}, drag] = useDrag({
+        type: type,
+        item: data,
+        collect: monitor => ({
+            opacity: monitor.isDragging() ? 0.6 : 1
+        })
+    });
+    return (
+        <div className={classes.default_question} ref={drag} style={{opacity}} key={data.id}>
+            {data.question?.questionType ? translations.questions[data.question.questionType].titleText : translations[data.type].titleText}
+        </div>
+    );
 };
 export default RightNavbarItem;
