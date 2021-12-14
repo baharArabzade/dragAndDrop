58c16ceefad9b715ea84d4642d168f22afec753c
bahar <bahar1377.a@gmail.com>
2021-12-15 00:26:32 +0330

    make QuestionsDraggable.

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useDrag} from "react-dnd";
 //style
 import classes from "styles/components/question.module.scss";
 // types
@@ -10,13 +11,17 @@ import {translations as englishTranslations} from "translations/english";
 const RenderQuestion = ({question}: RenderQuestionPropsTypes): JSX.Element => {
     const [language, setLanguage] = useState<string>('persian')
     const translations = (language === 'english' ? englishTranslations : persianTranslations)
+    const [{}, drag] = useDrag({
+        type: 'questionList',
+        item: question,
+    });
+    console.log(question)
     return (
-        <div
-            className={`${classes.question_row}`}
-        >
-            {translations.questions[question.questionType].titleText}
-        </div>
-
+        <>
+            <div className={`${classes.question_row}`} ref={drag}>
+                {translations.questions[question.questionType].titleText}
+            </div>
+        </>
 
     );
 };
