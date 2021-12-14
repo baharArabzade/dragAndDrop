1f1702e739c22d0261562cacaccaad39e54fca33
bahar <bahar1377.a@gmail.com>
2021-12-15 00:16:47 +0330

    ChangeSomeTypes.

--- types/componentType.ts
+++ types/componentType.ts
@@ -1,36 +1,53 @@
 import React from "react";
 
-export type RenderQuestionPropsTypes={
-    question:{
-        id:string;
-        questionType:string
-    };
-    index:number;
+export type RenderQuestionPropsTypes = {
+    question: {
+        id: string;
+        questionType?: string
+        subQuestions?: Array<{
+            id: string;
+            questionType: string;
+        }>
+    }
+    index?: number;
 }
-export type RightNavbarPropsTypes={
-    data:dragItemType;
-    type:string
+export type RightNavbarPropsTypes = {
+    data: dragItemType;
+    type: string
 }
-export type DropZonePropsTypes={
-    addQuestionFunction?:(questionType:string) => void;
+export type DropZonePropsTypes = {
+    addQuestionFunction?: (questionType: string) => void;
     children?: React.ReactNode;
-    acceptType:string
+    acceptType: Array<string> | string
+    addAppreciationPage?: (appreciationPageId: string) => void,
+    addWelcomePage?: (welcomePageId: string) => void,
+    addSubQuestionToGroupQuestion?: (subQuestion: { id: string, questionType: string }, groupQuestionId: string) => void
+    type: string
+    className: string
+    groupQuestionId?: string
+
 }
-export type welcomePageDetailsType= Array<{
-  id:string,
+export type welcomePageDetailsType = Array<{
+    id: string,
+    questionType: string
 }>
-export type appreciationPageDetailsType= Array<{
-    id:string,
+export type appreciationPageDetailsType = Array<{
+    id: string,
+    questionType: string
 }>
-export type questionsDetailsType=Array<{
-    id:string;
-    questionType:string
+export type questionsDetailsType = Array<{
+    id: string;
+    questionType: string
+    subQuestions?: Array<{
+        id: string;
+        questionType: string;
+    }>
 }>
-export type dragItemType={
-    id:string;
-    type:string;
-    question?:{
-        questionType:string;
-        id:string;
+export type dragItemType = {
+    id: string;
+    type: string;
+    question?: {
+        questionType: string;
+        id: string;
     }
 }
